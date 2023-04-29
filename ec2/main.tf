4e0a8bc7b41c0776256813e332da09690a9973ae
shuja <shuja.gmail.com>
2023-04-29 07:20:27 +0530

    ansible-pull for applications

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,9 @@
+data "aws_caller_identity" "current" {}
+
 data "aws_ami" "ami" {
   most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
+  name_regex       = "devops-practice-with-ansible"
+  owners           = [data.aws_caller_identity.current.account_id]
 }
 
 resource "aws_security_group" "sg" {
@@ -46,9 +48,10 @@ resource "null_resource" "provisioner" {
     }
 
     inline = [
-      "git clone https://github.com/shuja-git/roboshop-shell",
-      "cd roboshop-shell",
-      "sudo bash ${var.component}.sh ${var.password}"
+#      "git clone https://github.com/shuja-git/roboshop-shell",
+#      "cd roboshop-shell",
+#      "sudo bash ${var.component}.sh ${var.password}"
+      "ansible-pull -i localhost, -U https://github.com/shuja-git/roboshop-ansible-1 roboshop.yml -e role_name=${var.component}"
     ]
 
   }
